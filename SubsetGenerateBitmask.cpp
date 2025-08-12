3982353feac2dd52881e323828bbc98548ca791f
Nabil <nabilhossain282002@gmail.com>
2025-08-12 17:11:21 +0600

    Added

---
+++ SubsetGenerateBitmask.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    vector<int> arr = {2, 4, 5};
+    int n = arr.size();
+    int total_subset = (1 << n);
+    for (int mask = 0; mask < total_subset; mask++) // 2^n
+    {
+        vector<int> subset;
+        for (int i = 0; i < n; i++) // n
+        {
+            if (mask & (1 << i)) // if i-th bit is set
+                subset.push_back(arr[i]);
+        }
+
+        // Print the subset
+        for (int pr : subset)
+            cout << pr << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// time complexity : O(n * 2^n)
